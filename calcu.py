2b24f9f46da8dd6ef63cd2d42f3268a46340cbf0
NikhilT26 <115657483+NikhilT26@users.noreply.github.com>
2022-10-13 17:27:19 +0530

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -51,6 +51,8 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+        elif choice == '5':
+            print("NoChoice:") 
         
         # check if user wants another calculation
         next_calculation = input("Let's do next calculation? (yes/no): ")
