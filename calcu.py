8267b359ad0c319757f866dbf510e661374196aa
NikhilT26 <115657483+NikhilT26@users.noreply.github.com>
2022-10-13 17:28:26 +0530

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -37,7 +37,7 @@ while True:
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4','5'):
-        num1 = float(input("Enter first number: "))
+        num1 = float(input("Enter 1st number: "))
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
